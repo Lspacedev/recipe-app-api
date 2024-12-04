5ebc5cc62a514fb6db84455278b6883f021382a7
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-12-04 17:34:43 +0200

    prod

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function mongooseConnection() {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI_PROD);
     console.log(`MongoDb connected to: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error}`);
